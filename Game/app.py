b95cfe9d48e0a7bdb47e887ba6980767dd51f1ad
PolishCowPog <156892491+PolishCowPog@users.noreply.github.com>
2025-03-25 14:06:10 +0100

    updated flask to include all relevant html files

--- Game/app.py
+++ Game/app.py
@@ -10,6 +10,18 @@ app = Flask(__name__)
 def root():
     return render_template('index.html')
 
+@app.route('/store')
+def store():
+    return render_template('store.html')
+
+@app.route('/battle')
+def battle():
+    return render_template('battle.html')
+
+@app.route('/map')
+def map():
+    return render_template('map.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
