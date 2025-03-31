e744fd0b424acf6c1f1c9acba17ac579b298a953
PolishCowPog <156892491+PolishCowPog@users.noreply.github.com>
2025-03-31 13:12:27 +0200

    started on battle system and items.json

--- Game/static/js/script.js
+++ Game/static/js/script.js
@@ -1,9 +1,28 @@
 var gold = 0;
 var diamonds = 0;
 
-function onstart() {
-    document.getElementById("gold").innerHTML = "Gold " + gold;
-    document.getElementById("diamonds").innerHTML = "Diamonds " +  diamonds;
+if (window.location.pathname !== '/battle') {
+    function onstart() {
+        document.getElementById("gold").innerHTML = "Gold " + gold;
+        document.getElementById("diamonds").innerHTML = "Diamonds " +  diamonds;
+    }
+
+    onstart();
 }
 
-onstart();
+
+
+fetch('/static/json/items.json')
+    .then(response => response.json()) // Parse the JSON data
+    .then(items => {
+        console.log(items); // Log the items to the console
+
+        // Example: Display the first item's name
+        //document.body.innerHTML += `<p>First Item: ${items[0].name}</p>`;
+    })
+    .catch(error => console.error("Error fetching items.json:", error));
+
+
+if (window.location.pathname === '/battle') {
+    console.log("hello words")
+}
